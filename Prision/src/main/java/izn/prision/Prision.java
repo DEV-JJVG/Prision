ce380664ff901beaff9f016d4bf4e457e19ee9a0
Hugo <aunqueDiganQueSoyunBandoleroADondeVoy@gmail.com>
2025-04-01 12:32:48 +0200

    modificacion

---
+++ Prision/src/main/java/izn/prision/Prision.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package izn.prision;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Prision {
+
+    public static void main(String[] args) {
+        System.out.println("aeorigvpiusbgstrj");
+    }
+}
