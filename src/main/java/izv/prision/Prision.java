d2c1a439773e6d77a700b5dc0669f862800ada1a
JJ <DEV-JJVG@users.noreply.github.com>
2025-04-01 11:03:29 +0200

    Inicio del programa

---
+++ src/main/java/izv/prision/Prision.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package izv.prision;
+
+/**
+ *
+ * @author Mañana
+ */
+public class Prision {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
