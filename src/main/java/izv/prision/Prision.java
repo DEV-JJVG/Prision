fb5ab41d5d65420070b4c58850817828ce92f5fa
JJ <DEV-JJVG@users.noreply.github.com>
2025-04-01 12:27:04 +0200

    Modifico el texto de inicio

--- src/main/java/izv/prision/Prision.java
+++ src/main/java/izv/prision/Prision.java
@@ -11,6 +11,6 @@ package izv.prision;
 public class Prision {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Tetas Gordas!");
     }
 }
