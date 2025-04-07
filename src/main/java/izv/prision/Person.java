cb0626e86b24aea2a8662a41e0ccf38a49829b88
JJ <DEV-JJVG@users.noreply.github.com>
2025-04-07 09:24:36 +0200

    Atributos getters y setters y ToString de Person

--- src/main/java/izv/prision/Person.java
+++ src/main/java/izv/prision/Person.java
@@ -4,10 +4,51 @@
  */
 package izv.prision;
 
+import java.util.Date;
+
 /**
  *
  * @author Hugo
  */
 public class Person {
-    
+
+    protected String name;
+    protected Date born_date;
+    protected int id;
+
+    public Person(String name, Date born_date) {
+        this.name = name;
+        this.born_date = born_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        name.toUpperCase();
+        this.name = name;
+    }
+
+    public Date getBorn_date() {
+        return born_date;
+    }
+
+    public void setBorn_date(Date born_date) {
+        this.born_date = born_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", born_date=" + born_date + ", id=" + id + '}';
+    }
+
 }
