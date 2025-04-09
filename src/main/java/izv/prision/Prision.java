8351bfc6a78a3a4ed947de1fa6b69a77b7457051
Hugo <Dev-HugoBeznar@users.noreply.github.com>
2025-04-09 09:15:59 +0200

    area del main organizada para la puesta de funciones y prueba de clase cell añadida

--- src/main/java/izv/prision/Prision.java
+++ src/main/java/izv/prision/Prision.java
@@ -4,15 +4,77 @@
  */
 package izv.prision;
 
+import java.util.Scanner;
+
 /**
  *
  * @author JJ y Hugo
  */
 public class Prision {
 
-    
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("aibsc");
+        manualCellcreation();
     }
+
+    /*
     
+    
+    
+    
+    
+    
+    
+    empty espace to make the main look prettier
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     */
+    //normal funtions
+    //here there will be oall the funtions normaly used in the program
+    /*
+    
+    
+    
+    
+    
+    
+    
+     */
+    //manual creation of obgets,
+    //here the will be funtions that will create obgets in the program itself, and with 
+    //no prtections from erros, they will only be used to test the obgets
+    //and are only meantto be used by the programer
+    //the will be call manual to make clear what they do, other similar funtions
+    //meants for the user will be call "user______" and will be put in the section above
+    public static Cell manualCellcreation() {
+        System.out.println("int cellID, Integer cellNumber, String location, Integer CellCapacity");
+        int cellID = input.nextInt();
+        int cellNumber = input.nextInt();
+        input.nextLine(); 
+        String location = input.nextLine();
+        int cellCapacity = input.nextInt();
+        Cell cell = new Cell(cellID, cellNumber, location, cellCapacity);
+        System.out.println(cell);
+        return cell;
+    }
+
+    /*
+    
+    
+    
+    
+    
+     */
+    //automatic creation of obgets
+    //here there will be funtions that will create obgets automaticly and ramdomly
+    //they will be used when a valid obget is nededd and witch atributtes doesn't matter
 }
