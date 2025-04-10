c19ca9b5a95a106ea07b9f07fbcf917c77e2883f
Hugo <Dev-HugoBeznar@users.noreply.github.com>
2025-04-10 10:54:05 +0200

    hago el inmate mas organizado

--- src/main/java/Inmate/Inmate.java
+++ src/main/java/Inmate/Inmate.java
@@ -23,7 +23,7 @@ public class Inmate extends Person {
     boolean reoffending = false;
     ArrayList<Visit> visits;
 
-    public Inmate(Date entrance_date, Date exit_date, boolean status, String felony, ArrayList<Visit> visits, String name, Date born_date, Date current_date) throws InvalidAgeException{
+    public Inmate(Date entrance_date, Date exit_date, boolean status, String felony, ArrayList<Visit> visits, String name, Date born_date, Date current_date) throws InvalidAgeException {
         super(name, born_date, current_date);
         this.entrance_date = entrance_date;
         this.exit_date = exit_date;
@@ -32,78 +32,95 @@ public class Inmate extends Person {
         this.visits = visits;
     }
 
+    //
+    //
+    //
+    //getters
     public Date getEntrance_date() {
         return entrance_date;
-    }
-
-    public void setEntrance_date(Date entrance_date) {
-        this.entrance_date = entrance_date;
     }
 
     public Date getExit_date() {
         return exit_date;
     }
 
-    public void setExit_date(Date exit_date) {
-        this.exit_date = exit_date;
-    }
-
     public boolean isStatus() {
         return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
     }
 
     public String getFelony() {
         return felony;
     }
 
-    public void setFelony(String felony) {
-        this.felony = felony;
-    }
-
     public boolean isReoffending() {
         return reoffending;
-    }
-
-    public void setReoffending(boolean reoffending) {
-        this.reoffending = reoffending;
     }
 
     public ArrayList<Visit> getVisits() {
         return visits;
     }
 
-    public void setVisits(ArrayList<Visit> visits) {
-        this.visits = visits;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Date getBorn_date() {
         return born_date;
     }
 
-    public void setBorn_date(Date born_date) {
-        this.born_date = born_date;
-    }
-
     public int getId() {
         return id;
+    }
+
+    //
+    //
+    //
+    //setters
+    public void setEntrance_date(Date entrance_date) {
+        this.entrance_date = entrance_date;
+    }
+
+    public void setExit_date(Date exit_date) {
+        this.exit_date = exit_date;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setFelony(String felony) {
+        this.felony = felony;
+    }
+
+    public void setReoffending(boolean reoffending) {
+        this.reoffending = reoffending;
+    }
+
+    public void setVisits(ArrayList<Visit> visits) {
+        this.visits = visits;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBorn_date(Date born_date) {
+        this.born_date = born_date;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    //
+    //
+    //
+    //methods
+    
+    //
+    //
+    //
+    //toString
     @Override
     public String toString() {
         //Terminar toString de las visitas
