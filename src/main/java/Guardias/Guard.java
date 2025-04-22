980bb62128c4f690585588bf9cbbd9b0fd7a9078
Hugo <Dev-HugoBeznar@users.noreply.github.com>
2025-04-22 11:09:34 +0200

    le he hecho lo muy minimo y he cambiado del diseño el schedule y he quitado las otras clases

--- src/main/java/Guardias/Guard.java
+++ src/main/java/Guardias/Guard.java
@@ -4,10 +4,48 @@
  */
 package Guardias;
 
+import Exception.InvalidAgeException;
+import izv.prision.Person;
+import java.util.Date;
+import java.util.HashMap;
+
 /**
  *
  * @author Hugo
  */
-public class Guard {
+public class Guard extends Person implements MandatoryTasks{
+    enum CARGO{
+        director,
+        subdirector,
+        securityCheif,
+        captain,
+        liutenant,
+        sargeant,
+        oficer,
+    }
+    
+    HashMap<String, Activity> schedule;
+
+    public Guard(HashMap<String, Activity> schedule, String name, Date born_date) throws InvalidAgeException {
+        super(name, born_date);
+        setSchedule(schedule);
+    }
+
+    //getters
+
+    public HashMap<String, Activity> getSchedule() {
+        return schedule;
+    }
+    
+    //
+    //
+    //
+    //
+    //setters
+
+    public void setSchedule(HashMap<String, Activity> schedule) {
+        this.schedule = schedule;
+    }
+    
     
 }
